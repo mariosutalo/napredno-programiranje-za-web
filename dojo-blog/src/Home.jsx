be1345991e62ccbd6a9bcd0a93557f93d5b8ab1a
Mario Sutalo <mario.sutalo@gmail.com>
2025-01-27 13:39:51 +0100

    -cp

--- dojo-blog/src/Home.jsx
+++ dojo-blog/src/Home.jsx
@@ -2,11 +2,7 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+  const [blogs, setBlogs] = useState(null)
 
   const [name, setName] = useState('mario');
 
@@ -15,22 +11,28 @@ const Home = () => {
     setBlogs(newBlogs);
   }
 
-  // useEffect(()=> {
-  //   console.log("useeffect ran")
-  //   // go to api and fetch data to display
-  // }, [])
+  useEffect(() => {
+    // go to api and fetch data to display
+    fetch('http://localhost:8000/blogs')
+      .then((res) => (res.json()))
+      .then((data) => {
+        setBlogs(data)
+        console.log(data)
+      })
+      console.log("hello")
+  }, [])
 
   useEffect(() => {
     console.log('new name is:', name);
   }, [name])
-  
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
       <button onClick={() => setName('luigi')}>change name</button>
-      <input type="text" onChange={(e)=>(setName(e.target.value))}/>
+      <input type="text" onChange={(e) => (setName(e.target.value))} />
     </div>
   );
 }
- 
+
 export default Home;
