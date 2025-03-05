06243aa0bb94e162fa9af8be0969a3c1f103f9c0
Mario Sutalo <mario.sutalo@gmail.com>
2025-03-05 11:16:47 +0100

    -cp

--- dojo-blog/src/components/Create.jsx
+++ dojo-blog/src/components/Create.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Create = () => {
+
+
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const [author, setAuthor] = useState('yoshi')
+
+
   return (
-    <div>Add a New Blog</div>
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form action="">
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => { setBody(e.target.value) }}
+        />
+
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="mario">Mario</option>
+          <option value="yoshi">Yoshi</option>
+        </select>
+        <button>Add blog</button>
+      </form>
+    </div>
   )
 }
 
