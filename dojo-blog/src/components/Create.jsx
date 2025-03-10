31aa27fb44346fd6acd533644297f598aca7bb3c
Mario Sutalo <mario.sutalo@gmail.com>
2025-03-10 13:07:10 +0100

    -cp

--- dojo-blog/src/components/Create.jsx
+++ dojo-blog/src/components/Create.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
 const Create = () => {
 
@@ -7,12 +8,28 @@ const Create = () => {
   const [title, setTitle] = useState('')
   const [body, setBody] = useState('')
   const [author, setAuthor] = useState('yoshi')
+  const [isPending, setIsPending] = useState(false)
+  const history = useHistory()
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const blog = { title, body, author }
+    setIsPending(true)
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('New blog added')
+      setIsPending(false)
+      history.push('/')
+    })
+  }
 
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
           type="text"
@@ -36,7 +53,9 @@ const Create = () => {
           <option value="mario">Mario</option>
           <option value="yoshi">Yoshi</option>
         </select>
-        <button>Add blog</button>
+        {!isPending && <button>Add blog</button>}
+        {isPending && <button disabled>Adding blog...</button>}
+
       </form>
     </div>
   )
