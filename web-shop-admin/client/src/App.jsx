319ed5eb2bedf092b4e21b732b1058d2b463f423
Mario Šutalo <mario.sutalo@gmail.com>
2025-03-23 18:48:54 +0100

    -cp

--- web-shop-admin/client/src/App.jsx
+++ web-shop-admin/client/src/App.jsx
@@ -1,16 +1,25 @@
 import './assets/styles/global.css'
-import Header from './components/Header/Header'
-import Products from './components/Products/Products';
-import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+import Layout from './components/Layout/Layout';
+import Login from './Pages/Login/Login';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Product from './Pages/Products/Products';
+import Categories from './Pages/Categories/Categories';
+import User from './Pages/Users/Users';
+import Order from './Pages/Orders/Orders';
 
 function App() {
 
   return (
     <Router>
-      <Header />
       <Routes>
-        <Route path={"/"} element={<Products />} />
-        <Route path={"/products"} element={<Products />} />
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/"} element={<Layout />}>
+          <Route index element={<Product/>}/>
+          <Route path="products" element={<Product/>}/>
+          <Route path="categories" element={<Categories/>}/>
+          <Route path="users" element={<User/>}/>
+          <Route path="orders" element={<Order/>}/>
+        </Route>
       </Routes>
     </Router>
   )
