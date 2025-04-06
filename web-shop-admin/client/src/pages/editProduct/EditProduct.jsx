c5605c64d6cd9d55b81a0156e2642662cb881015
Mario Šutalo <mario.sutalo@gmail.com>
2025-04-06 11:43:11 +0200

    -cp

--- web-shop-admin/client/src/pages/editProduct/EditProduct.jsx
+++ web-shop-admin/client/src/pages/editProduct/EditProduct.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import styles from './EditProduct.module.css'
 
 const EditProduct = () => {
-    const params = useParams()
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+    }
+
     return (
-        <div>EditProduct with id: {params.productId}</div>
+        <div className={styles.container_div}>
+            <form className={styles.product_form} onSubmit={handleSubmit}>
+            <h1 className={styles.page_title_h1}>Edit product</h1>
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name"/>
+                <label htmlFor="price">Price</label>
+                <input type="text" id="price"/>
+                <label htmlFor="stock">Stock</label>
+                <input type="text" id="stock"/>
+                <label htmlFor="categoryId">Category id</label>
+                <input type="text" id="categoryId"/>
+                <label htmlFor="productDescription">Description</label>
+                <textarea id="productDescription"></textarea>
+                <button type='submit'>Save changes</button>
+            </form>
+        </div>
     )
 }
 
