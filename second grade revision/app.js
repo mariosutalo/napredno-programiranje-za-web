4cefc75ca1f45255d0bab302f7e4c80881e6b180
Mario Šutalo <mario.sutalo@gmail.com>
2025-09-19 13:04:16 +0200

    -cp

---
+++ second grade revision/app.js
@@ -0,0 +1,8 @@
+function changeText() {
+    const p1 = document.getElementById('text-description1')
+    p1.innerText = 'Changed text!'
+    const p2 = document.getElementById('text-description2')
+    p2.innerText = 'Changed text!'
+    const p3 = document.getElementById('text-description3')
+    p3.innerText = 'Changed text!'
+}
