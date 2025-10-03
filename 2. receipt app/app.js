73ceb6c32f2a25b7913273da8a42ac530eb97a1d
Mario Šutalo <mario.sutalo@gmail.com>
2025-10-03 12:12:14 +0200

    -cp

--- 2. receipt app/app.js
+++ 2. receipt app/app.js
@@ -1,3 +1,4 @@
+const receiptItems = []
 function addItemToReceipt() {
     const itemNameInput = document.getElementById('item-name')
     const itemName = itemNameInput.value
@@ -7,8 +8,32 @@ function addItemToReceipt() {
 
     const itemQuantityInput = document.getElementById('item-quantity')
     const itemQuantity = itemQuantityInput.value
-
     const itemTotalPrice = Number(itemPrice) * Number(itemQuantity)
-    const itemDescriptionParagraph = document.getElementById('item-description')
-    itemDescriptionParagraph.innerText = `Naziv artikla: ${itemName}, cijena: ${itemPrice}, količina: ${itemQuantity}, ukupan iznos stavke je ${itemTotalPrice}`
+    const receiptItem = {
+        name: itemName,
+        price: itemPrice,
+        quantity: itemQuantity,
+        total: itemTotalPrice
+    }
+    receiptItems.push(receiptItem)
+    addReceiptItemsToTable()
+}
+
+function addReceiptItemsToTable() {
+    const table = document.getElementById('products-table')
+    const tableHeader = `<tr>
+                <th>Naziv artikla</th>
+                <th>Cijena</th>
+                <th>Količina</th>
+                <th>Ukupno</th>
+            </tr>`
+    const itemStartTr = '<tr>'
+    const nameColumn = `<td>Mlijeko</td>`
+    const priceColumn = `<td>2</td>`
+    const quantityColumn = `<td>3</td>`
+    const totalPriceColumn = `<td>6</td>`
+    const itemEndTr = '</tr>'
+
+    table.innerHTML = tableHeader + itemStartTr + nameColumn
+    + priceColumn + quantityColumn + totalPriceColumn + itemEndTr
 }
