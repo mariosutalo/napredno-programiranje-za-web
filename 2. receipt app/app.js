51bdbfc15a374024e71398f217703fd665395db9
Mario Šutalo <mario.sutalo@gmail.com>
2025-10-17 12:14:57 +0200

    -cp

--- 2. receipt app/app.js
+++ 2. receipt app/app.js
@@ -1,4 +1,5 @@
 const receiptItems = []
+let receiptItemsCounter = 1
 function addItemToReceipt() {
     const itemNameInput = document.getElementById('item-name')
     const itemName = itemNameInput.value
@@ -10,13 +11,16 @@ function addItemToReceipt() {
     const itemQuantity = itemQuantityInput.value
     const itemTotalPrice = Number(itemPrice) * Number(itemQuantity)
     const receiptItem = {
+        id: receiptItemsCounter,
         name: itemName,
         price: itemPrice,
         quantity: itemQuantity,
         total: itemTotalPrice
     }
     receiptItems.push(receiptItem)
+    receiptItemsCounter++
     addReceiptItemsToTable()
+    calculateTotalPrice()
 }
 
 function addReceiptItemsToTable() {
@@ -26,14 +30,33 @@ function addReceiptItemsToTable() {
                 <th>Cijena</th>
                 <th>Količina</th>
                 <th>Ukupno</th>
+                <th>Brisanje</th>
             </tr>`
-    const itemStartTr = '<tr>'
-    const nameColumn = `<td>Mlijeko</td>`
-    const priceColumn = `<td>2</td>`
-    const quantityColumn = `<td>3</td>`
-    const totalPriceColumn = `<td>6</td>`
-    const itemEndTr = '</tr>'
+    let tableHtml = tableHeader
+    for (let index = 0; index < receiptItems.length; index++) {
+        const receiptItem = receiptItems[index];
+        tableHtml += `<tr>
+        <td>${receiptItem.name}</td>
+        <td>${receiptItem.price}</td>
+        <td>${receiptItem.quantity}</td>
+        <td>${receiptItem.price * receiptItem.quantity}</td>
+        <td><button onclick="deleteItem(3)">Obriši</button></td>
+        </tr>`
+    }
+    table.innerHTML = tableHtml
+}
 
-    table.innerHTML = tableHeader + itemStartTr + nameColumn
-    + priceColumn + quantityColumn + totalPriceColumn + itemEndTr
+function calculateTotalPrice() {
+    let totalPrice = 0
+    for (let index = 0; index < receiptItems.length; index++) {
+        const item = receiptItems[index];
+        const itemTotal = item.price * item.quantity
+        totalPrice += itemTotal
+    }
+    const p = document.getElementById('total-price')
+    p.innerHTML = `Total price: ${totalPrice}`
+}
+
+function deleteItem(itemId) {
+
 }
