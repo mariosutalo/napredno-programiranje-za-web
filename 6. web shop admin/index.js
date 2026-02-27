e63c3d1315f8bca1ea5441892441844657335d33
Mario Šutalo <mario.sutalo@gmail.com>
2026-02-27 12:55:44 +0100

    -cp

--- 6. web shop admin/index.js
+++ 6. web shop admin/index.js
@@ -1,45 +1,47 @@
-import express from 'express'
-import mysql from 'mysql2'
+import express from "express";
+import mysql from "mysql2";
 
 const dbConnection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'password',
-    database: 'shop'
-})
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "shop",
+});
 
 dbConnection.connect((error) => {
-    if (error) {
-        console.log('error connecting to db:', error)
-        return
-    }
+  if (error) {
+    console.log("error connecting to db:", error);
+  }
+});
 
-    dbConnection.query(
-        `select name, price, stock
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  dbConnection.query(
+    `select name, price, stock
         from product;`,
-        (error, results, fields) => {
-            if (error) {
-                console.log('error executing query', error)
-                return
-            }
-            console.log('simple select data', results)
-        }
-    )
-})
+    (error, results, fields) => {
+      if (error) {
+        console.log("error executing query", error);
+        res.render("server-error");
+        return;
+      }
+      console.log("simple select data", results);
+      res.render("index", {
+        pageName: "Products",
+        products: results,
+      });
+    },
+  );
+});
+app.get("/orders", (req, res) => {
+  res.render("orders", { pageName: "Orders" });
+});
+app.get("/users", (req, res) => {
+  res.render("users", { pageName: "Users" });
+});
 
-const app = express()
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-    res.render('index', { pageName: 'Products' })
-})
-app.get('/orders', (req, res) => {
-    res.render('orders', { pageName: 'Orders' })
-})
-app.get('/users', (req, res) => {
-    res.render('users', { pageName: 'Users' })
-})
-
-app.listen(3001)
+app.listen(3001);
