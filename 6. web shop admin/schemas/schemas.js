39bbcfbca7c72c0c790945abee025bbc23675822
Mario Šutalo <mario.sutalo@gmail.com>
2026-05-22 12:38:09 +0200

    -cp

--- 6. web shop admin/schemas/schemas.js
+++ 6. web shop admin/schemas/schemas.js
@@ -2,8 +2,8 @@ import * as z from "zod";
 
 export const addNewProductSchema = z.object({
     name: z.string().min(3),
-    categoryId: z.int().positive(),
-    stock: z.number().nonnegative(),
-    price: z.number().nonnegative(),
+    categoryId: z.coerce.number().positive(),
+    stock: z.coerce.number().nonnegative(),
+    price: z.coerce.number().nonnegative(),
     description: z.string().optional()
 })
